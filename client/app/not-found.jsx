73b46bfe786f2f1b5fe2ebb3713a1e7a75b5d916
Marco <formante.marco@gmail.com>
2026-02-20 22:27:25 +0000

    create notFound page with h1 and p tag

---
+++ client/app/not-found.jsx
@@ -0,0 +1,14 @@
+"use client"
+export default function NotFound(){
+    return (
+        <div className="notFound-container">
+            <div>
+                <h1 className="notfound-h1">404</h1>
+                <p>Il semble que la page que vous cherchez ait pris des vacances… ou n’ait jamais existé.</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
