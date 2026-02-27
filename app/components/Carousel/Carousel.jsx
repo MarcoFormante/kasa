c15becd6225bfffe194bdb192e8901d5f46868f2
Marco <formante.marco@gmail.com>
2026-02-27 21:50:35 +0000

    create carousel button component

--- app/components/Carousel/Carousel.jsx
+++ app/components/Carousel/Carousel.jsx
@@ -2,6 +2,7 @@
 'use client'
 import Image from "next/image"
 import { useEffect, useRef, useState } from "react"
+import { ArrowButton } from "./ArrowButton";
 
 export function Carousel({images,closeCarousel}){
 
@@ -41,6 +42,8 @@ export function Carousel({images,closeCarousel}){
     return () => clearTimeout(timeoutRef.current);
   }, [currentIndex, slides.length]);
 
+  
+
   useEffect(()=>{
     window.addEventListener("keydown",(e)=>{
         if (e.key === "Escape" ) {
@@ -53,7 +56,6 @@ export function Carousel({images,closeCarousel}){
             closeCarousel()
         }
     })
-
   },[closeCarousel])
 
     return (
@@ -79,20 +81,8 @@ export function Carousel({images,closeCarousel}){
                 </div>
                
             </div>
-              <button aria-label="Slide precedente" onClick={prevSlide} className="carousel-arrow carousel-arrow-left">
-                    <svg width="64px" height="64px" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
-                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
-                        <g id="SVGRepo_iconCarrier"> <path d="M13 15L16 12M16 12L13 9M16 12H8M7.2 20H16.8C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V7.2C20 6.0799 20 5.51984 19.782 5.09202C19.5903 4.71569 19.2843 4.40973 18.908 4.21799C18.4802 4 17.9201 4 16.8 4H7.2C6.0799 4 5.51984 4 5.09202 4.21799C4.71569 4.40973 4.40973 4.71569 4.21799 5.09202C4 5.51984 4 6.07989 4 7.2V16.8C4 17.9201 4 18.4802 4.21799 18.908C4.40973 19.2843 4.71569 19.5903 5.09202 19.782C5.51984 20 6.07989 20 7.2 20Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/> </g>
-                    </svg>
-                </button>
-                <button aria-label="Slide suivante"  onClick={nextSlide} className="carousel-arrow carousel-arrow-right">
-                    <svg width="64px" height="64px" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
-                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
-                        <g id="SVGRepo_iconCarrier"> <path d="M13 15L16 12M16 12L13 9M16 12H8M7.2 20H16.8C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V7.2C20 6.0799 20 5.51984 19.782 5.09202C19.5903 4.71569 19.2843 4.40973 18.908 4.21799C18.4802 4 17.9201 4 16.8 4H7.2C6.0799 4 5.51984 4 5.09202 4.21799C4.71569 4.40973 4.40973 4.71569 4.21799 5.09202C4 5.51984 4 6.07989 4 7.2V16.8C4 17.9201 4 18.4802 4.21799 18.908C4.40973 19.2843 4.71569 19.5903 5.09202 19.782C5.51984 20 6.07989 20 7.2 20Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/> </g>
-                    </svg>
-                </button>
+                <ArrowButton aria={"Slide precedente"} onClick={prevSlide} style={"carousel-arrow-left"}/>
+                <ArrowButton aria={"Slide suivante"} onClick={nextSlide} style={"carousel-arrow-right"}/>
         </div>
     )
 }
